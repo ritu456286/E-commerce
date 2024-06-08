a10d4cb73a49a63466cce921998588cd0a031e1f
Ritu Kansal <ritu.kansal456@gmail.com>
2024-06-09 02:31:34 +0530

    update our menu-items to navigate using React Hooks in react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import HomePage from './pages/homepage.component';
+
+import { Route, Routes } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route exact path='/hats' element={<HatsPage />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import './menu-item.styles.scss'
+import './menu-item.styles.scss';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
-        <div className="content">
-            <h1 className="title">{ title } </h1>
-            <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleClick = () => {
+        navigate(`${location.pathname}${linkUrl}`);
+    };
+
+    return (
+        <div className={`${size} menu-item`} onClick={handleClick}>
+            <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})`
+            }} />
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
-    </div>
-)
+    );
+};
 
 export default MenuItem;
