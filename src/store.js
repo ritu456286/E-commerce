67960d171f67ec497254a15e0dcafb7c02ff7de8
Ritu Kansal <ritu.kansal456@gmail.com>
2024-06-28 01:29:31 +0530

    Added Redux Persist local Storage to persist the cart items even on refreshing the page

--- src/store.js
+++ src/store.js
@@ -1,14 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import userReducer from './features/user/userSlice';
 import cartReducer from './features/cart/cartSlice'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
