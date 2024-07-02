822ddd0206bd35e405b27fce78b6c4c8ebe73bb0
Ritu Kansal <ritu.kansal456@gmail.com>
2024-07-02 17:25:01 +0530

    Converted some components sass styles to react js styles using styled-components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,50 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/crown.svg.svg";
-import "./header.styles.scss";
-import { auth } from "../../firebase/firebase.utils";
-import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { useSelector } from "react-redux";
+
 import { selectCurrentUser } from "../../features/user/userSelector";
 import { selectCartHidden } from "../../features/cart/cartSelectors";
+import { auth } from "../../firebase/firebase.utils";
 
+import { ReactComponent as Logo } from "../../assets/crown.svg.svg";
+
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+import { HeaderContainer, OptionsContainer,LogoContainer, OptionLink } from "./header.styles";
 
 const Header = () => {
   const currentUser = useSelector(selectCurrentUser);
   const hidden = useSelector(selectCartHidden);
 
   return(
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo" />
-    </Link>
+  <HeaderContainer>
+    <LogoContainer to="/" >
+      <Logo />
+    </LogoContainer>
 
-    <div className="options">
-      <Link className="option" to="/shop">
+    <OptionsContainer>
+      <OptionLink to="/shop">
         SHOP
-      </Link>
-      <Link className="option" to="/contact">
+      </OptionLink>
+      <OptionLink to="/contact">
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ? 
-        <div className="option" onClick={() => auth.signOut()}> 
+        <OptionLink as='div' onClick={() => auth.signOut()}> 
         SIGN OUT
-        </div>
+        </OptionLink>
         :
-        (<Link className="option" to="/signin">
+        (<OptionLink to="/signin">
         SIGN IN
-      </Link>)
+      </OptionLink>)
       }
       <CartIcon />  
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />} 
-  </div>
+  </HeaderContainer>
   );
 };
-
-
-
 
 export default Header;
